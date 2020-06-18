d876f24f29e0540e1853af3c61dc7beb6c03fe3d
thomasRita <thomasrita98@yahoo.com>
2020-06-18 09:16:48 +0100

    integrate swagger

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from 'express'
-import morganLogger from 'morgan';
-import bodyParser from 'body-parser';
-import routes from './routes';
+import morganLogger from 'morgan'
+import bodyParser from 'body-parser'
+import swaggerUi from 'swagger-ui-express'
+import swagger from './swagger'
+import routes from './routes'
 
 const app = express()
 
-app.use(morganLogger('dev'));
+app.use(morganLogger('dev'))
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger))
 
-app.use('/', routes); 
+app.use('/', routes)
 
 app.get('/', (req, res) =>  {
     return res.status(200).json({ message: 'Creating APIs' })
