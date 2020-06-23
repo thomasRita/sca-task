c4ac18a508827840c6f1bd5409c9a268e1c6782b
thomasRita <thomasrita98@yahoo.com>
2020-06-23 15:47:15 +0100

    update db config

--- db/index.js
+++ db/index.js
@@ -5,9 +5,16 @@ import logger from '../services/logger'
 
 dotenv.config()
 
+let connectionString;
+if (process.env.NODE_ENV === "development") {
+  connectionString = process.env.DATABASE_DEV_URL;
+} else {
+  connectionString = process.env.DATABASE_PRODUCTION_URL;
+}
+
 
 const db = new Pool({
-    connectionString: process.env.DATABASE_DEV_URL
+    connectionString
   });
 
   db.on('connect', () => {
